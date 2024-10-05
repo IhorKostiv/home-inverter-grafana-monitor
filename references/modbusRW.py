cd5db065ec83c126928a472e6428bdfdf27732ac
IhorKostiv <ihor.kostiv@gmai.com>
2024-10-05 16:18:18 +0300

    writing

---
+++ references/modbusRW.py
@@ -0,0 +1,27 @@
+import time
+import minimalmodbus
+import sys
+
+device_id = int(sys.argv[1])
+baud_rate = int(sys.argv[2])
+register = int(sys.argv[3])
+value = int(sys.argv[4])
+
+SERPORT = '/dev/ttyUSB0'
+SERTIMEOUT = 0.5
+SERBAUD = baud_rate
+
+i = minimalmodbus.Instrument(SERPORT, device_id)
+i.serial.timeout= SERTIMEOUT
+i.serial.baudrate = SERBAUD
+i.debug = True
+i.clear_buffers_before_each_transaction = True
+
+result = i.read_register(register)
+print("{0} was {1}".format(i + register, result))
+
+#if value != "":
+#    time.sleep(0.1)
+
+#    i.write_register(register, value)
+#    print("{0} now {1}".format(i + register, value))
