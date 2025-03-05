d93b1b21bfd2dd88a4571f02a55e227c664d8d5c
IhorKostiv <ihor.kostiv@gmai.com>
2025-03-05 18:41:11 +0200

    save battery charge

--- monitor/ups/__init__.py
+++ monitor/ups/__init__.py
@@ -143,7 +143,7 @@ class UPSmgr(UPS): # base class for smarter solar power and battery management (
                 #elif : # more than equalization and pv > avg(on, off) meaning battery is overcharged
             case "SBU": # PV full production mode
                 if solarVoltageOff < 1 or self.pvVoltage < solarVoltageOff: # better to be more sophisticated formula accounting MPPT since voltage depend on produced power
-                    if self.iPGrid >= self.iPLoad and self.pvChargerPower < self.iPLoad: # solar power not enough
+                    if (self.iPGrid >= self.iPLoad or self.iBattPower > self.pvChargerPower) and self.pvChargerPower < self.iPLoad: # solar power not enough
                         print(f"Set Solar Off by Power {self.iPGrid} >= {self.iPLoad} > {self.pvChargerPower}")
                         return self.saveBattery()
                     elif self.iBatteryVoltage < (self.icBatteryStopCharging + self.icBatteryStopDischarging) / 2: # battery is draining
